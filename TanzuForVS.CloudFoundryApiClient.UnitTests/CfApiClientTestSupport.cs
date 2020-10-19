0453b8709a992d90c7105a1649c78bfbf574f15f
atwoosnam <andrewt.woosnam@gmail.com>
2020-10-19 16:23:04 -0400

    Add CfApiClientTestSupport for fake api responses
    - Condense BasicInfoResponse Href props

---
+++ TanzuForVS.CloudFoundryApiClient.UnitTests/CfApiClientTestSupport.cs
@@ -0,0 +1,71 @@
+﻿using TanzuForVS.CloudFoundryApiClient.Models;
+using TanzuForVS.CloudFoundryApiClient.Models.BasicInfoResponse;
+using TanzuForVS.CloudFoundryApiClient.Models.OrgsResponse;
+
+namespace TanzuForVS.CloudFoundryApiClient.UnitTests
+{
+    class FakeBasicInfoResponse : BasicInfoResponse
+    {
+        public FakeBasicInfoResponse(string loginHref, string uaaHref) : base()
+        {
+            links = new Models.BasicInfoResponse.Links
+            {
+                login = new Href
+                {
+                    href = loginHref
+                },
+                uaa = new Href
+                {
+                    href = uaaHref
+                }
+            };
+        }
+    }
+
+    class FakeOrgsResponse : OrgsResponse
+    {
+        public FakeOrgsResponse(string apiAddress, int pageNum, int totalResults, int totalPages, int resultsPerPage) : base()
+        {
+            bool isFirstPage = pageNum == 1;
+            bool isLastPage = pageNum == totalPages;
+
+            var firstHref = new Href() { href = $"{apiAddress}{CfApiClient.listOrgsPath}?page=1&per_page={resultsPerPage}" };
+            var lastHref = new Href() { href = $"{apiAddress}{CfApiClient.listOrgsPath}?page={totalPages}&per_page={resultsPerPage}" };
+            var nextHref = isLastPage ? null : new Href() { href = $"{apiAddress}{CfApiClient.listOrgsPath}?page={pageNum + 1}&per_page={resultsPerPage}" };
+            var previousHref = isFirstPage ? null : new Href() { href = $"{apiAddress}{CfApiClient.listOrgsPath}?page={pageNum - 1}&per_page={resultsPerPage}" };
+
+            pagination = new Models.OrgsResponse.Pagination
+            {
+                total_results = totalResults,
+                total_pages = totalPages,
+                first = firstHref,
+                last = lastHref,
+                next = nextHref,
+                previous = previousHref
+            };
+
+            Resource[] resourcesArr;
+            if (isLastPage)
+            {
+                int numResourcesInLastPage = totalResults % resultsPerPage;
+                resourcesArr = new Resource[numResourcesInLastPage];
+
+                for (int i = 0; i < numResourcesInLastPage; i++)
+                {
+                    resourcesArr[i] = new Resource($"fakeOrg{i + 1}");
+                }
+            }
+            else
+            {
+                resourcesArr = new Resource[resultsPerPage];
+
+                for (int i = 0; i < resultsPerPage; i++)
+                {
+                    resourcesArr[i] = new Resource($"fakeOrg{i + 1}");
+                }
+            }
+
+            resources = resourcesArr;
+        }
+    }
+}
--- TanzuForVS.CloudFoundryApiClient/Models/BasicInfoResponse.cs
+++ TanzuForVS.CloudFoundryApiClient/Models/BasicInfoResponse.cs
@@ -8,25 +8,20 @@
 
     public class Links
     {
-        public Self self { get; set; }
+        public Href self { get; set; }
         public object bits_service { get; set; }
         public Cloud_Controller_V2 cloud_controller_v2 { get; set; }
         public Cloud_Controller_V3 cloud_controller_v3 { get; set; }
-        public Network_Policy_V0 network_policy_v0 { get; set; }
-        public Network_Policy_V1 network_policy_v1 { get; set; }
-        public Login login { get; set; }
-        public Uaa uaa { get; set; }
+        public Href network_policy_v0 { get; set; }
+        public Href network_policy_v1 { get; set; }
+        public Href login { get; set; }
+        public Href uaa { get; set; }
         public object credhub { get; set; }
-        public Routing routing { get; set; }
-        public Logging logging { get; set; }
-        public Log_Cache log_cache { get; set; }
-        public Log_Stream log_stream { get; set; }
+        public Href routing { get; set; }
+        public Href logging { get; set; }
+        public Href log_cache { get; set; }
+        public Href log_stream { get; set; }
         public App_Ssh app_ssh { get; set; }
-    }
-
-    public class Self
-    {
-        public string href { get; set; }
     }
 
     public class Cloud_Controller_V2
@@ -49,46 +44,6 @@
     public class Meta1
     {
         public string version { get; set; }
-    }
-
-    public class Network_Policy_V0
-    {
-        public string href { get; set; }
-    }
-
-    public class Network_Policy_V1
-    {
-        public string href { get; set; }
-    }
-
-    public class Login
-    {
-        public string href { get; set; }
-    }
-
-    public class Uaa
-    {
-        public string href { get; set; }
-    }
-
-    public class Routing
-    {
-        public string href { get; set; }
-    }
-
-    public class Logging
-    {
-        public string href { get; set; }
-    }
-
-    public class Log_Cache
-    {
-        public string href { get; set; }
-    }
-
-    public class Log_Stream
-    {
-        public string href { get; set; }
     }
 
     public class App_Ssh
