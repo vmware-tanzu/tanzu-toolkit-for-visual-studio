112498ec17f37a12a65368e634c4b7090293b785
atwoosnam <andrewt.woosnam@gmail.com>
2020-10-19 17:43:24 -0400

    Add Pagination model
    - reducing duplicate code in other models

--- TanzuForVS.CloudFoundryApiClient/Models/OrgsResponse.cs
+++ TanzuForVS.CloudFoundryApiClient/Models/OrgsResponse.cs
@@ -10,16 +10,6 @@ namespace TanzuForVS.CloudFoundryApiClient.Models.OrgsResponse
         public Org[] Orgs { get; set; }
     }
 
-    public class Pagination
-    {
-        public int total_results { get; set; }
-        public int total_pages { get; set; }
-        public Href first { get; set; }
-        public Href last { get; set; }
-        public Href next { get; set; }
-        public Href previous { get; set; }
-    }
-
     public class Org
     {
         public string guid { get; set; }
--- TanzuForVS.CloudFoundryApiClient/Models/SpacesResponse.cs
+++ TanzuForVS.CloudFoundryApiClient/Models/SpacesResponse.cs
@@ -12,17 +12,6 @@ namespace TanzuForVS.CloudFoundryApiClient.Models.SpacesResponse
         public Space[] Spaces { get; set; }
     }
 
-    public class Pagination
-    {
-        public int total_results { get; set; }
-        public int total_pages { get; set; }
-        public Href first { get; set; }
-        public Href last { get; set; }
-        public Href next { get; set; }
-        public Href previous { get; set; }
-    }
-
-
     public class Space
     {
         public string guid { get; set; }
