fd5351f74461ba5345560eebd623277ecb8a230f
Andrew T Woosnam <andrewt.woosnam@gmail.com>
2021-04-27 15:01:25 -0400

    Throw exception when cf.exe not found

--- Services.Tests/FileLocator/FileLocatorServiceTests.cs
+++ Services.Tests/FileLocator/FileLocatorServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.IO;
 using Tanzu.Toolkit.VisualStudio.Services.FileLocator;
 
@@ -26,7 +27,7 @@ namespace Tanzu.Toolkit.VisualStudio.Services.Tests.FileLocator
         public void CliVersion_CanBeSetTo6()
         {
             Assert.AreEqual(7, sut.CliVersion);
-            
+
             sut.CliVersion = 6;
             Assert.AreEqual(6, sut.CliVersion);
         }
@@ -36,7 +37,7 @@ namespace Tanzu.Toolkit.VisualStudio.Services.Tests.FileLocator
         {
             sut.CliVersion = 6;
             Assert.AreEqual(6, sut.CliVersion);
-            
+
             sut.CliVersion = 7;
             Assert.AreEqual(7, sut.CliVersion);
         }
@@ -45,7 +46,7 @@ namespace Tanzu.Toolkit.VisualStudio.Services.Tests.FileLocator
         public void CliVersion_CannotBeSetToAnythingBut6Or7()
         {
             Assert.AreEqual(7, sut.CliVersion);
-            
+
             sut.CliVersion = 5;
 
             Assert.AreEqual(7, sut.CliVersion);
@@ -54,7 +55,7 @@ namespace Tanzu.Toolkit.VisualStudio.Services.Tests.FileLocator
         [TestMethod]
         public void FullPathToCfExe_ReturnsV6Path_WhenCliVersionIs6()
         {
-            
+
             sut.CliVersion = 6;
 
             var path = sut.FullPathToCfExe;
@@ -71,6 +72,30 @@ namespace Tanzu.Toolkit.VisualStudio.Services.Tests.FileLocator
             var path = sut.FullPathToCfExe;
 
             Assert.IsTrue(path.Contains("cf7.exe"));
+        }
+
+        [TestMethod]
+        public void FullPathToCfExe_ThrowsException_WhenExecutableFileNotFound()
+        {
+            var fakeAssemblyBaseDir = "/fake/path";
+            sut = new FileLocatorService(fakeAssemblyBaseDir)
+            {
+                CliVersion = 7
+            };
+
+            Exception expectedException = null;
+
+            try
+            {
+                var path = sut.FullPathToCfExe;
+            }
+            catch (Exception ex)
+            {
+                expectedException = ex;
+            }
+
+            Assert.IsNotNull(expectedException);
+            Assert.IsTrue(expectedException.Message.Contains(sut.CliVersion.ToString()));
         }
 
     }
