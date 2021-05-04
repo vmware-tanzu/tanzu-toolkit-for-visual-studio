8f2d1b7876d1e8f11886eccf1d531afc1a51b26e
Andrew T Woosnam <andrewt.woosnam@gmail.com>
2021-05-04 10:44:00 -0400

    Rename VisualStudioService -> VsToolWindowService : IViewService
    - This makes it clearer that the WpfViews project does not depend on
      anything specific to Visual Studio; it depends on an IViewService
      which must expose a "DisplayView" method & this VSIX package decides
      to implement IViewService with its own VsToolWindowService.

--- WpfViews/IVisualStudioService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Tanzu.Toolkit.VisualStudio.WpfViews.Services
-{
-    public interface IVisualStudioService
-    {
-        void DisplayToolWindowForView(Type viewType);
-    }
-}
