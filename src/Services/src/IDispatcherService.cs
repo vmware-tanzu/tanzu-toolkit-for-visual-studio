2825ed305c115d6a89bb12aebe82b9ce640b055e
Andrew T Woosnam <andrewt.woosnam@gmail.com>
2021-05-20 17:11:56 -0400

    Add background refresh poller
    - new DispatcherService which can start the poller on the UI thread

---
+++ src/Services/src/IDispatcherService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Tanzu.Toolkit.Services
+{
+    public interface IDispatcherService
+    {
+        void StartUiBackgroundPoller(Func<object, Task> method, object methodParam, int intervalInSeconds);
+        void StopUiBackgroundUiPoller();
+    }
+}
---
+++ src/WpfViews/src/Services/WpfDispatcherService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using Tanzu.Toolkit.Services;
+
+namespace Tanzu.Toolkit.VisualStudio.WpfViews.Services
+{
+    public class WpfDispatcherService : IDispatcherService
+    {
+        private bool _isPolling = false;
+
+        public void StartUiBackgroundPoller(Func<object, Task> method, object methodParam, int intervalInSeconds)
+        {
+            var uiDispatcher = Application.Current.Dispatcher;
+
+            if (!_isPolling)
+            {
+                _isPolling = true;
+
+                var pollingTask = new Task(async () =>
+                {
+                    while (_isPolling)
+                    {
+                        await Task.Delay(TimeSpan.FromSeconds(intervalInSeconds));
+
+                        await uiDispatcher.InvokeAsync(async () =>
+                        {
+                            await method(methodParam);
+                        });
+                    }
+                });
+
+                pollingTask.Start();
+            }
+        }
+
+        public void StopUiBackgroundUiPoller()
+        {
+            if (_isPolling)
+            {
+                _isPolling = false;
+            }
+        }
+    }
+}
