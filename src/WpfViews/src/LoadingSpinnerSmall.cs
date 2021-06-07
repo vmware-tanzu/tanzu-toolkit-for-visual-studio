08a1e3fe88466a0592958a9d3045ae689c67901f
Andrew T Woosnam <andrewt.woosnam@gmail.com>
2021-06-07 12:52:50 -0400

    Replace "Refreshing ..." text with spinners

---
+++ src/WpfViews/src/LoadingSpinnerSmall.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Tanzu.Toolkit.VisualStudio.WpfViews
+{
+    public class LoadingSpinnerSmall : Control
+    {
+        public static readonly DependencyProperty IsLoadingProperty = 
+            DependencyProperty.Register("IsLoading", typeof(bool), typeof(LoadingSpinnerSmall), 
+                new PropertyMetadata(false));
+
+        public bool IsLoading
+        {
+            get { return (bool)GetValue(IsLoadingProperty); }
+            set { SetValue(IsLoadingProperty, value);  }
+        }
+
+        static LoadingSpinnerSmall()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LoadingSpinnerSmall), new FrameworkPropertyMetadata(typeof(LoadingSpinnerSmall)));
+        }
+    }
+}
