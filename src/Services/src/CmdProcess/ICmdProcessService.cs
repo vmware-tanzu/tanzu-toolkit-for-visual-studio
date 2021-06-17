e8631b6c81b6a1be428600fe401341b3dc01746b
Andrew T Woosnam <andrewt.woosnam@gmail.com>
2021-06-17 16:01:59 -0400

    Remove unused method: InvokeWindowlessCommandAsync

--- src/Services/src/CmdProcess/ICmdProcessService.cs
+++ src/Services/src/CmdProcess/ICmdProcessService.cs
@@ -6,7 +6,6 @@ namespace Tanzu.Toolkit.Services.CmdProcess
     public interface ICmdProcessService
     {
         CmdResult ExecuteWindowlessCommand(string arguments, string workingDir);
-        Task<CmdResult> InvokeWindowlessCommandAsync(string arguments, string workingDir, StdOutDelegate stdOutDelegate, StdErrDelegate stdErrDelegate);
         CmdResult RunCommand(string executableFilePath, string arguments, string workingDir, StdOutDelegate stdOutDelegate, StdErrDelegate stdErrDelegate);
     }
 }
