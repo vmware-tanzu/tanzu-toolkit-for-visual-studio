1f5385f6f70e8426bad4cd21d49ea28e1e3f21de
Andrew T Woosnam <andrewt.woosnam@gmail.com>
2021-06-17 16:15:04 -0400

    Replace ExecuteWindowlessCommand with RunCommand

--- src/Services/src/CmdProcess/ICmdProcessService.cs
+++ src/Services/src/CmdProcess/ICmdProcessService.cs
@@ -1,11 +1,9 @@
-﻿using System.Threading.Tasks;
-using static Tanzu.Toolkit.Services.OutputHandler.OutputHandler;
+﻿using static Tanzu.Toolkit.Services.OutputHandler.OutputHandler;
 
 namespace Tanzu.Toolkit.Services.CmdProcess
 {
     public interface ICmdProcessService
     {
-        CmdResult ExecuteWindowlessCommand(string arguments, string workingDir);
         CmdResult RunCommand(string executableFilePath, string arguments, string workingDir, StdOutDelegate stdOutDelegate, StdErrDelegate stdErrDelegate);
     }
 }
