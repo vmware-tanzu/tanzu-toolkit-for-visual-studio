a70217081f3cab1a831d5de07193088b5711a4a3
Andrew T Woosnam <andrewt.woosnam@gmail.com>
2021-07-09 15:27:08 -0400

    Remove AccessToken prop from CloudFoundryInstance
    - also remove call to CfCliService.GetOAuthToken from CloudFoundryService.ConnectToCfAsync

--- src/Services/src/CloudFoundry/ConnectResult.cs
+++ src/Services/src/CloudFoundry/ConnectResult.cs
@@ -2,15 +2,13 @@
 {
     public class ConnectResult
     {
-        public ConnectResult(bool loggedIn, string errorMessage, string token)
+        public ConnectResult(bool loggedIn, string errorMessage)
         {
             IsLoggedIn = loggedIn;
             ErrorMessage = errorMessage;
-            Token = token;
         }
 
         public bool IsLoggedIn { get; }
         public string ErrorMessage { get; }
-        public string Token { get; set; }
     }
 }
