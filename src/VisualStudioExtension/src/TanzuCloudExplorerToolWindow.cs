5a2b0e456a753df51d3d92ce13621f9f1208ebf9
Andrew T Woosnam <andrewt.woosnam@gmail.com>
2021-07-21 15:44:45 -0400

    "Tanzu Cloud Explorer" -> "Tanzu Application Service Explorer"

--- src/VisualStudioExtension/src/TanzuCloudExplorerToolWindow.cs
+++ src/VisualStudioExtension/src/TanzuCloudExplorerToolWindow.cs
@@ -25,7 +25,7 @@ namespace Tanzu.Toolkit.VisualStudio
         /// <param name="view"></param>
         public TanzuCloudExplorerToolWindow(ICloudExplorerView view) : base(null)
         {
-            Caption = "Tanzu Cloud Explorer";
+            Caption = "Tanzu Application Service Explorer";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
