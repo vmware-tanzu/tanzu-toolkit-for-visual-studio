b3bd7e2955dfca649031ca4b3f17e39fe8c33906
Zayd Zori <zzori@vmware.com>
2021-09-29 15:40:57 -0400

    Add Buildpack model to CfApiClient [#165]

---
+++ src/CloudFoundryApiClient/src/Models/BuildpacksResponse.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Tanzu.Toolkit.CloudFoundryApiClient.Models
+{
+    public class BuildpacksResponse
+    {
+        [JsonProperty("pagination")]
+        public Pagination Pagination { get; set; }
+
+        [JsonProperty("resources")]
+        public Buildpack[] Buildpacks { get; set; }
+    }
+
+    public class Buildpack
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("stack")]
+        public string Stack { get; set; }
+    }
+}
