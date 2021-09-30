1d58b8478f9c16df992c7f675eb51268490d1381
Andrew T Woosnam <andrewt.woosnam@gmail.com>
2021-09-30 15:21:15 -0400

    Remove CfCli get orgs/spaces/apps methods
    - this extension instead uses the CloudFoundryApiClient to get orgs/spaces/apps for these reasons:
      - performance (users of this extension will notice faster response times if making direct HTTP requests instead of invoking the CF CLI & parsing stdout)
      - accuracy (parsing HTTP responses is less error prone than parsing std output from the CF CLI & the format of HTTP responses is standardized/unlikely to change, which can't be said for the structure of CF CLI output)
      - the CF CLI exists in this extension as a convienient way to perform CF operations that are difficult to orchestrate manually (examples: pushing an app / authenitcating). Ideally this extension would not have a dependency on the CF CLI (because the executable is large & its API is subject to change), so the fewer CF CLI methods we depend on the better.

--- src/Services/src/CfCli/ICfCliService.cs
+++ src/Services/src/CfCli/ICfCliService.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Security;
 using System.Threading.Tasks;
-using Tanzu.Toolkit.Services.CfCli.Models.Apps;
-using Tanzu.Toolkit.Services.CfCli.Models.Orgs;
-using Tanzu.Toolkit.Services.CfCli.Models.Spaces;
 using static Tanzu.Toolkit.Services.OutputHandler.OutputHandler;
 
 namespace Tanzu.Toolkit.Services.CfCli
@@ -15,11 +11,8 @@ namespace Tanzu.Toolkit.Services.CfCli
         DetailedResult TargetApi(string apiAddress, bool skipSsl);
         Task<DetailedResult> AuthenticateAsync(string username, SecureString password);
         DetailedResult ExecuteCfCliCommand(string arguments, string workingDir = null);
-        Task<DetailedResult<List<Org>>> GetOrgsAsync();
-        Task<DetailedResult<List<Space>>> GetSpacesAsync();
         DetailedResult TargetOrg(string orgName);
         DetailedResult TargetSpace(string spaceName);
-        Task<DetailedResult<List<App>>> GetAppsAsync();
         Task<DetailedResult> StopAppByNameAsync(string appName);
         Task<DetailedResult> StartAppByNameAsync(string appName);
         Task<DetailedResult> DeleteAppByNameAsync(string appName, bool removeMappedRoutes = true);
