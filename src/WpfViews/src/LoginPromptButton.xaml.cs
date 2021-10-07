08eafbd07d5dbd48c46b37f048791f28c41ec3dd
Andrew T Woosnam <andrewt.woosnam@gmail.com>
2021-10-07 10:03:47 -0400

    Extract login button content as separate user control

---
+++ src/WpfViews/src/LoginPromptButton.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Tanzu.Toolkit.VisualStudio.WpfViews
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class LoginPromptButton : UserControl
+    {
+        public LoginPromptButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
