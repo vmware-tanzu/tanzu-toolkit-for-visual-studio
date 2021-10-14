4043c0ba325a2958b1e388db874a2c2e4dbe2ee4
Andrew T Woosnam <andrewt.woosnam@gmail.com>
2021-10-14 09:22:30 -0400

    Validate api address url via ValidationRule [#173]

---
+++ src/WpfViews/src/Errors/UrlValidationRule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace Tanzu.Toolkit.VisualStudio.WpfViews.Errors
+{
+    public class UrlValidationRule : ValidationRule
+    {
+        public UrlValidationRule()
+        {
+        }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            return value is string apiAddress
+                ? string.IsNullOrWhiteSpace(apiAddress)
+                    ? new ValidationResult(false, "Invalid Api Address: cannot be empty.")
+                    : Uri.IsWellFormedUriString(apiAddress, UriKind.Absolute)
+                        ? ValidationResult.ValidResult
+                        : new ValidationResult(false, "Invalid Api Address: URI format could not be determined.")
+                : new ValidationResult(false, "Invalid Api Address: cannot parse input.");
+        }
+    }
+}
