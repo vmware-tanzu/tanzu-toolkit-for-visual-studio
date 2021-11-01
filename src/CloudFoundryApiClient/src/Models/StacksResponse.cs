07f306fe59a503494da6980d04fe19ae878f0d79
Andrew T Woosnam <andrewt.woosnam@gmail.com>
2021-11-01 16:13:39 -0400

    Use Newtonsoft for StacksResponse deserialization [#169]

--- src/CloudFoundryApiClient/src/Models/StacksResponse.cs
+++ src/CloudFoundryApiClient/src/Models/StacksResponse.cs
@@ -1,23 +1,23 @@
-﻿using System.Runtime.Serialization;
+﻿using Newtonsoft.Json;
 
 namespace Tanzu.Toolkit.CloudFoundryApiClient.Models.StacksResponse
 {
     public class StacksResponse
     {
-        [DataMember(Name = "pagination")]
+        [JsonProperty("pagination")]
         public Pagination Pagination { get; set; }
 
-        [DataMember(Name = "resources")]
+        [JsonProperty("resources")]
         public Stack[] Stacks { get; set; }
     }
 
 
     public class Stack
     {
-        [DataMember(Name = "guid")]
+        [JsonProperty("guid")]
         public string Guid { get; set; }
 
-        [DataMember(Name = "name")]
+        [JsonProperty("name")]
         public string Name { get; set; }
 
     }
