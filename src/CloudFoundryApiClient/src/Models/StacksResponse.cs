2a193e685e12d6e9d6d2b65d9b590c32441451a5
Andrew T Woosnam <andrewt.woosnam@gmail.com>
2021-11-01 14:14:48 -0400

    Add method CfApiClient.ListStacks [#169]

---
+++ src/CloudFoundryApiClient/src/Models/StacksResponse.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+
+namespace Tanzu.Toolkit.CloudFoundryApiClient.Models.StacksResponse
+{
+    public class StacksResponse
+    {
+        [DataMember(Name = "pagination")]
+        public Pagination Pagination { get; set; }
+
+        [DataMember(Name = "resources")]
+        public Stack[] Stacks { get; set; }
+    }
+
+
+    public class Stack
+    {
+        [DataMember(Name = "guid")]
+        public string Guid { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+    }
+}
+
