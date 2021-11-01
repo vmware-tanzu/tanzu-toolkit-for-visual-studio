f30830ed410291c12d79683b2ccfba91213cbbc4
Andrew T Woosnam <andrewt.woosnam@gmail.com>
2021-11-01 15:06:27 -0400

    New method: CfService.GetStackNamesAsync [#169]

--- src/Services/src/CloudFoundry/ICloudFoundryService.cs
+++ src/Services/src/CloudFoundry/ICloudFoundryService.cs
@@ -20,5 +20,6 @@ namespace Tanzu.Toolkit.Services.CloudFoundry
         DetailedResult CreateManifestFile(string location, AppManifest manifest);
         DetailedResult<AppManifest> ParseManifestFile(string pathToManifestFile);
         Task<DetailedResult> DeployAppAsync(AppManifest appManifest, CloudFoundryInstance targetCf, CloudFoundryOrganization targetOrg, CloudFoundrySpace targetSpace, StdOutDelegate stdOutCallback, StdErrDelegate stdErrCallback);
+        Task<DetailedResult<List<string>>> GetStackNamesAsync(CloudFoundryInstance cf, int retryAmount = 1);
     }
 }
