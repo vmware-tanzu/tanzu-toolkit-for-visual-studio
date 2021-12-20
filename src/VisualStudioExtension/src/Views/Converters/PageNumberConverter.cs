a75ac84b64dce319d3ca551ec5726dee1c93ced2
Andrew T Woosnam <awoosnam@vmware.com>
2021-12-20 12:17:14 -0800

    Paginate LoginView [#195]

---
+++ src/VisualStudioExtension/src/Views/Converters/PageNumberConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Tanzu.Toolkit.VisualStudio.Views.Converters
+{
+    public class PageNumberConverter : IValueConverter
+    {
+        public int ShowValue { get; set; }
+        public bool ReserveSpace { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value as int?) == ShowValue ? Visibility.Visible : ReserveSpace ? Visibility.Hidden : (object)Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException("Visibility Converter can only be used OneWay.");
+        }
+    }
+}
