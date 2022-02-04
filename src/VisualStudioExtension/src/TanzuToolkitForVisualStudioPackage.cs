323d4b189ab08cc3ca785b3cf4b6a8fd2f33eef9
Andrew T Woosnam <awoosnam@vmware.com>
2022-02-04 11:20:58 -0500

    Prevent log stream windows from reopening [#131]

--- src/VisualStudioExtension/src/TanzuToolkitForVisualStudioPackage.cs
+++ src/VisualStudioExtension/src/TanzuToolkitForVisualStudioPackage.cs
@@ -50,7 +50,7 @@ namespace Tanzu.Toolkit.VisualStudio
     [Guid(PackageGuidString)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideToolWindow(typeof(TanzuTasExplorerToolWindow))]
-    [ProvideToolWindow(typeof(OutputToolWindow), MultiInstances = true)]
+    [ProvideToolWindow(typeof(OutputToolWindow), MultiInstances = true, Transient = true)]
     public sealed class TanzuToolkitForVisualStudioPackage : ToolkitPackage
     {
         /// <summary>
