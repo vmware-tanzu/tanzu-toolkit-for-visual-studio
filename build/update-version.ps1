8af48308de6489da155806fdbccb1471b1f35554
Tim Hess <thess@vmware.com>
2022-02-09 10:20:44 -0500

    pipeline tweaks for multiple vs versions

--- build/update-version.ps1
+++ build/update-version.ps1
@@ -1,10 +1,17 @@
 # Thanks to Gérald Barré for the template of this script (source: https://www.meziantou.net/ci-cd-pipeline-for-a-visual-studio-extension-vsix-using-azure-devops.htm)
 
 $version = $args[0] -replace "-build", ""
-Write-Host "Set version: $version"
 
-$FullPath = Resolve-Path $PSScriptRoot\..\src\VisualStudioExtension\src\source.extension.vsixmanifest
-Write-Host $FullPath
-[xml]$content = Get-Content $FullPath
+Write-Host "Setting version in manifest files to $version"
+
+$VS2019Path = Resolve-Path $PSScriptRoot\vs2019\source.extension.vsixmanifest
+Write-Host "Updating Visual Studio 2019 manifest at $VS2019Path"
+[xml]$content = Get-Content $VS2019Path
 $content.PackageManifest.Metadata.Identity.Version = $version
-$content.Save($FullPath)
+$content.Save($VS2019Path)
+
+$VS2022Path = Resolve-Path $PSScriptRoot\vs2022\source.extension.vsixmanifest
+Write-Host "Updating Visual Studio 2022 manifest at $VS2022Path"
+[xml]$content = Get-Content $VS2022Path
+$content.PackageManifest.Metadata.Identity.Version = $version
+$content.Save($VS2022Path)
