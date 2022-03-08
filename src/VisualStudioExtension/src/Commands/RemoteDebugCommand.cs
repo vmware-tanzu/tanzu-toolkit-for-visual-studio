7e83fe5addb8ff7f15b52f4936f3c54ae0e96f42
Andrew T Woosnam <awoosnam@vmware.com>
2022-03-08 10:36:05 -0500

    Add remote dbg cmd to project context menu [#215]

---
+++ src/VisualStudioExtension/src/Commands/RemoteDebugCommand.cs
@@ -0,0 +1,15 @@
+﻿using Community.VisualStudio.Toolkit;
+using Microsoft.VisualStudio.Shell;
+using System.Threading.Tasks;
+
+namespace Tanzu.Toolkit.VisualStudio
+{
+    [Command("f91c88fb-6e17-42a6-878d-f4d16ead7625", 260)]
+    internal sealed class RemoteDebugCommand : BaseCommand<RemoteDebugCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await VS.MessageBox.ShowWarningAsync("RemoteDebugCommand", "Button clicked");
+        }
+    }
+}
