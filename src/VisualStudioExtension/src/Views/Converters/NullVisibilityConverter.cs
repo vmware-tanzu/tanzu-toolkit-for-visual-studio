285e519d7114d4af188bf0bb00984fb73ac4fc9e
Andrew T Woosnam <awoosnam@vmware.com>
2022-03-17 12:40:34 -0400

    Improve remote debug window rendering [#215]

---
+++ src/VisualStudioExtension/src/Views/Converters/NullVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Tanzu.Toolkit.VisualStudio.Views.Converters
+{
+    public class NullVisibilityConverter : IValueConverter
+    {
+        public bool Reversed { get; set; }
+        public bool ReserveSpace { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null
+                ? Reversed ? Visibility.Visible : ReserveSpace ? Visibility.Hidden : (object)Visibility.Collapsed
+                : Reversed ? ReserveSpace ? Visibility.Hidden : (object)Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException("Visibility Converter can only be used OneWay.");
+        }
+    }
+}
