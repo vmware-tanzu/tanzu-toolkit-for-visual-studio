a7b8ce001087148c9acee4e8737aa0756de9b6c1
Andrew T Woosnam <awoosnam@vmware.com>
2022-03-22 12:56:31 -0400

    Improve login api address error display
    - only show api address error text when there's an error; don't show blank space where the error would go if there's no error (collapse the whitespace)

---
+++ src/VisualStudioExtension/src/Views/Converters/StringNullVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Tanzu.Toolkit.VisualStudio.Views.Converters
+{
+    public class StringNullVisibilityConverter : IValueConverter
+    {
+        public bool Reversed { get; set; }
+        public bool ReserveSpace { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.IsNullOrWhiteSpace(value as string)
+                ? Reversed ? Visibility.Visible : ReserveSpace ? Visibility.Hidden : (object)Visibility.Collapsed
+                : Reversed ? ReserveSpace ? Visibility.Hidden : (object)Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException("Visibility Converter can only be used OneWay.");
+        }
+    }
+}
