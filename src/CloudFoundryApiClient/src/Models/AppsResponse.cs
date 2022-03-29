71dad8e927ff85a8cfb0ac81ca041acc8e82785f
Andrew T Woosnam <awoosnam@vmware.com>
2022-03-29 14:23:33 -0400

    Amend adapter args for windows stack [#215]
    - Add Lifecycle props to CfApiClient App model (specifically for "buildpacks" type)
    - Track stack on CloudFoundryApp

--- src/CloudFoundryApiClient/src/Models/AppsResponse.cs
+++ src/CloudFoundryApiClient/src/Models/AppsResponse.cs
@@ -21,5 +21,36 @@ namespace Tanzu.Toolkit.CloudFoundryApiClient.Models.AppsResponse
 
         [JsonPropertyName("state")]
         public string State { get; set; }
+        
+        [JsonPropertyName("lifecycle")]
+        public Lifecycle Lifecycle { get; set; }
+    }
+
+    public class Lifecycle
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+        
+        /** NOTE:
+         * Cloud Controller v3.116.0 supports 3 valid lifecycle "type" values:
+         * "buildpack"
+         * "docker"
+         * "kpack"
+         * 
+         * The following "Data" object is structured according to the "buildpack" 
+         * type; deserialization may not succeed for different types.
+         */
+
+        [JsonPropertyName("data")]
+        public Data Data { get; set; }
+    }
+
+    public class Data
+    {
+        [JsonPropertyName("buildpacks")]
+        public string[] Buildpacks { get; set; }
+
+        [JsonPropertyName("stack")]
+        public string Stack { get; set; }
     }
 }
