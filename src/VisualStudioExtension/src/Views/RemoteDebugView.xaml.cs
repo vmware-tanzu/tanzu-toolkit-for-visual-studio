6e30dd42f52ab32af146552bc80c715beebb8efb
Andrew T Woosnam <awoosnam@vmware.com>
2022-04-13 15:57:17 -0400

    Improve remote debug app selection visuals
    - Bold listview selection + gray border mouseover
    - Add nice bg colors to listView rows
    - Add background to listView header
    - Remove border from listView selections

--- src/VisualStudioExtension/src/Views/RemoteDebugView.xaml.cs
+++ src/VisualStudioExtension/src/Views/RemoteDebugView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.PlatformUI;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 using Tanzu.Toolkit.ViewModels.RemoteDebug;
 using Tanzu.Toolkit.VisualStudio.Services;
 using Tanzu.Toolkit.VisualStudio.Views;
@@ -16,6 +17,13 @@ namespace Tanzu.Toolkit.VisualStudio
         public ICommand CancelCommand { get; }
         public ICommand OpenLoginViewCommand { get; }
         public ICommand ResolveMissingAppCommand { get; }
+        public Brush ListItemMouseOverBrush { get { return (Brush)GetValue(ListItemMouseOverBrushProperty); } set { SetValue(ListItemMouseOverBrushProperty, value); } }
+        public Brush SelectedItemActiveBrush { get { return (Brush)GetValue(SelectedItemActiveBrushProperty); } set { SetValue(SelectedItemActiveBrushProperty, value); } }
+        public Brush GridHeaderBrush { get { return (Brush)GetValue(GridHeaderBrushProperty); } set { SetValue(GridHeaderBrushProperty, value); } }
+
+        public static readonly DependencyProperty ListItemMouseOverBrushProperty = DependencyProperty.Register("ListItemMouseOverBrush", typeof(Brush), typeof(RemoteDebugView), new PropertyMetadata(default(Brush)));
+        public static readonly DependencyProperty SelectedItemActiveBrushProperty = DependencyProperty.Register("SelectedItemActiveBrush", typeof(Brush), typeof(RemoteDebugView), new PropertyMetadata(default(Brush)));
+        public static readonly DependencyProperty GridHeaderBrushProperty = DependencyProperty.Register("GridHeaderBrushProperty", typeof(Brush), typeof(RemoteDebugView), new PropertyMetadata(default(Brush)));
 
         public RemoteDebugView(IRemoteDebugViewModel viewModel, IThemeService themeService)
         {
