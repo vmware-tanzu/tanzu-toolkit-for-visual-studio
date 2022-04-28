abb595871d90e85a109085655391dfe40682b01a
Andrew T Woosnam <awoosnam@vmware.com>
2022-04-28 14:31:32 -0400

    Add enter / escape key actions to applicable windows (#325)
    * Enable enter/escape key on AppDeletionConfimation window
    
    * Enable enter/escape key on deployment window
    
    * Enable enter/escape keys on Login view
    
    * Enable enter/escape keys on remote debug view

---
+++ src/VisualStudioExtension/src/Views/Converters/IntToBoolConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Tanzu.Toolkit.VisualStudio.Views.Converters
+{
+    public class IntToBoolConverter : IValueConverter
+    {
+        public int ExpectedInt { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value as int?) == ExpectedInt;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException("Visibility Converter can only be used OneWay.");
+        }
+    }
+}
