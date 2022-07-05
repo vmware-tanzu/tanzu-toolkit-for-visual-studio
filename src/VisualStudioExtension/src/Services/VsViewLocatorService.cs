8c19afc140698ad3cdba8c2a0f73a96e8318ac02
Andrew T Woosnam <awoosnam@vmware.com>
2022-07-05 15:58:42 -0400

    Fix ViewShownAsModal tests: include WPF in test proj

--- src/VisualStudioExtension/src/Services/VsViewLocatorService.cs
+++ src/VisualStudioExtension/src/Services/VsViewLocatorService.cs
@@ -66,23 +66,8 @@ namespace Tanzu.Toolkit.VisualStudio.Services
 
         internal bool ViewShownAsModal(Type type)
         {
-            return type == typeof(ILoginView)
-                || type == typeof(IDeploymentDialogView)
-                || type == typeof(IAppDeletionConfirmationView)
-                || type == typeof(IRemoteDebugView);
-
-            /** ^ this solution is admittedly not ideal because it will require updating when adding/changing modals.
-             * A preferred solution is shown below, but for some unknown reason the reference to AbstractModel prompts
-             * an error when running tests *specifically in VS 2019* (tests pass in VS 2022):
-             *      System.IO.FileNotFoundException: Could not load file or assembly 'PresentationFramework, 
-             *      Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'. The system cannot find the 
-             *      file specified.
-             * This error is bizarre because the PresentationFramework assembly (v4.0.0.0)  is definitely present
-             * 
-             *      var expectedImplementationType = Type.GetType(type.Namespace + "." + type.Name.Substring(1));
-             *      return expectedImplementationType != null && expectedImplementationType.IsSubclassOf(typeof(AbstractModal));
-             */
-
+            var expectedImplementationType = Type.GetType(type.Namespace + "." + type.Name.Substring(1));
+            return expectedImplementationType != null && expectedImplementationType.IsSubclassOf(typeof(AbstractModal));
         }
 
         internal bool ViewShownAsToolWindow(Type type)
