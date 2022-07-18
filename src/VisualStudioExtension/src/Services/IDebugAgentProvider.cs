9f82e9e5c18af57830d2936f3b90d763c4674d68
Andrew T Woosnam <awoosnam@vmware.com>
2022-07-18 15:19:18 -0400

    Add IDebugAgentProvider

---
+++ src/VisualStudioExtension/src/Services/IDebugAgentProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Tanzu.Toolkit.Models;
+
+namespace Tanzu.Toolkit.Services.DebugAgentProvider
+{
+    public interface IDebugAgentProvider
+    {
+        Task<DetailedResult> InstallVsdbgForCFAppAsync(CloudFoundryApp app, string vsVersion);
+    }
+}
