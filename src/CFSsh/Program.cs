12da441136207a9ebd2e87e164ab1a25a5572842
Andrew T Woosnam <awoosnam@vmware.com>
2022-07-26 14:18:28 -0400

    Add proj for remote debug adapter exe

---
+++ src/CFSsh/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+namespace CFSsh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 4)
+            {
+                Environment.Exit(1);
+            }
+
+            var pathToCfHome = args[0];
+            var pathToCfExe = args[1];
+            var appName = args[2];
+            var sshCmd = args[3];
+
+            var _cfSshProcess = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = pathToCfExe,
+                    Arguments = $"ssh {appName} -c \"{sshCmd}\"",
+                }
+            };
+
+            _cfSshProcess.StartInfo.EnvironmentVariables["CF_HOME"] = pathToCfHome;
+
+            if (!_cfSshProcess.Start())
+            {
+                Environment.Exit(2);
+            }
+
+            try
+            {
+                _cfSshProcess.WaitForExit();
+            }
+            catch
+            {
+                Environment.Exit(3);
+            }
+        }
+    }
+}
